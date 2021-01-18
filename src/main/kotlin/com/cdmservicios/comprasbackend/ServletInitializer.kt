bac88080e8df8a43824755a222eb7da64f442780
Jorge Mina <minajorge@hotmail.com>
2021-01-18 15:04:48 -0500

    fix: jar -> war

---
+++ src/main/kotlin/com/cdmservicios/comprasbackend/ServletInitializer.kt
@@ -0,0 +1,10 @@
+package com.cdmservicios.comprasbackend
+
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+
+class ServletInitializer : SpringBootServletInitializer() {
+    override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
+        return application.sources(ComprasbackendApplication::class.java)
+    }
+}
